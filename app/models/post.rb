a3ea8c063f80115c2b94e9254d973898262e5ca2
unclezap <zeb.blackwell@gmail.com>
2020-02-26 17:09:59 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validates :summary, length: { maximum: 250 }
+    validates_with PostValidator
+
 end
